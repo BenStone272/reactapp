0bdac39fca12e949b93ec9e1ea97b7be62ecd107
BenStone272 <BenStone272@gmail.com>
2023-09-18 21:52:24 -0400

    Added test user watchlist and movie focus

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from "react";
 import { useState } from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./NavBar";
+import { useNavigate } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 let posterSrc = "";
 let movieObj = [];
-let mainMovie = { type: "Fiat", model: "500", color: "white" };
+let movieWatchList = [];
+let selectedMovie;
+
 function App() {
   return (
     <div className="App">
-      <Movie />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Movie />} />
+        <Route path="/demo" element={<Demo />} />
+        <Route path="/watchlist" element={<Demo2 />} />
+        <Route path="/focus" element={<FocusMovie obj={selectedMovie} />} />
+      </Routes>
     </div>
   );
 }
@@ -18,7 +30,7 @@ function Movie() {
   const [poster, setPoster] = useState(posterSrc);
   const [name, setName] = useState("");
   const [plot, setPlot] = useState("");
-  Testprops2(setPoster);
+  const [release_date, setRelease_date] = useState("");
   function MovieButton() {
     const [val, setval] = useState(0);
 
@@ -39,30 +51,7 @@ function Movie() {
     setPlot(object.overview);
     console.log(posterSrc);
     setPoster(posterSrc);
-  }
-
-  function MovieList(obj) {
-    console.log(obj);
-    const [numbers, setNumbers] = useState(obj.lst);
-    function handleClick(obj) {
-      console.log("clicked movie " + obj.original_title);
-      ShowPoster(obj);
-    }
-
-    const listItems = numbers.map((numbers) => (
-      <li key={numbers.id}>
-        <div className="movieContainer">
-          <p>{numbers.original_title}</p>
-          <button onClick={(e) => handleClick(numbers)}>
-            <img
-              src={"http://image.tmdb.org/t/p/w500/" + numbers.poster_path}
-              width="180"
-              height="265"></img>
-          </button>
-        </div>
-      </li>
-    ));
-    return <ul>{listItems}</ul>;
+    setRelease_date("Released " + object.release_date.slice(0, 4));
   }
 
   return (
@@ -76,59 +65,17 @@ function Movie() {
         <div id="poster">
           <img className="avatar" src={poster} />
           <p>{name}</p>
+          <p>{release_date}</p>
           <p>{plot}</p>
         </div>
         <div id="plot" />
       </div>
-      <MovieList lst={movieObj} />
+      <MovieList lst={movieObj} ShowPoster={ShowPoster} />
     </div>
   );
-  function ReactTable(obj) {
-    const [lst, setLst] = useState(obj.lst);
-    console.log("test");
-    console.log(obj.lst);
-
-    return (
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Poster</th>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {lst.map((val, i) => (
-              <tr key={i}>
-                <td>
-                  <img
-                    src={
-                      "http://image.tmdb.org/t/p/w500/" + val.poster_path
-                    }></img>
-                </td>
-                <td>{val.original_title}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
 }
 
 export default App;
-
-function Testprops(props) {
-  props.setPoster("https://m.media-amazon.com/images/I/81RZipc6yOL.jpg");
-  console.log("prop tests");
-  return <p>test</p>;
-}
-
-function Testprops2(props) {
-  //props("https://m.media-amazon.com/images/I/81RZipc6yOL.jpg");
-  console.log("prop tests2");
-  console.log(props);
-}
 
 function searchMovie(title, showPoster) {
   let sParameter = encodeURIComponent(title.trim());
@@ -154,4 +101,95 @@ function searchMovie(title, showPoster) {
       console.log(movieObj);
     })
     .catch((err) => console.error(err));
+}
+
+function MovieList(obj) {
+  const navigate = useNavigate();
+  const tst = obj.lst;
+  const [numbers, setNumbers] = useState(tst);
+  console.log(numbers);
+  console.log("ddddddddddddddddddddddddddddddd");
+  function handleClick(obj2) {
+    console.log("clicked movie " + obj2.original_title);
+    obj.ShowPoster(obj2);
+    selectedMovie = obj2;
+    console.log(selectedMovie);
+    navigate("/focus");
+  }
+
+  const listItems = obj.lst.map((numbers) => (
+    <li key={numbers.id}>
+      <div className="movieContainer">
+        <p>{numbers.original_title}</p>
+        <button onClick={(e) => handleClick(numbers)}>
+          <img
+            src={"http://image.tmdb.org/t/p/w500/" + numbers.poster_path}
+            width="180"
+            height="265"></img>
+        </button>
+      </div>
+    </li>
+  ));
+  console.log(listItems);
+  return <ul>{listItems}</ul>;
+}
+
+function Demo() {
+  return <h1>This is the demo page</h1>;
+}
+
+function Demo2() {
+  return <Watchlist lst={movieWatchList} />;
+}
+
+function Watchlist(obj) {
+  const [lst, setLst] = useState([1]);
+  if (lst[0] == 1) {
+    setLst([2]);
+    getLst(setLst);
+  }
+  function handleClick() {
+    //setTimeout(setLst([3]), 3000);
+    setLst([2]);
+  }
+
+  return (
+    <>
+      <MovieList lst={movieWatchList} ShowPoster={obj.ShowPoster} />
+    </>
+  );
+}
+
+async function getLst(setLst) {
+  const lst = [78, 11791, 11801, 11826, 348350]; // would call api to get users watchlist here
+  let movieLst = [];
+  for (let i = 0; i < lst.length; i++) {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/" + lst[i] + "?language=en-US",
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YWFiMTViNWE0ZTFjZTYxMTY5ZGE1ZTc3Zjk3MjQ4NiIsInN1YiI6IjY0ZmZiMDMxZGI0ZWQ2MTAzMmE3MDViYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CF_vamypSa0m8oF06Hh1JSO7ajPkYf96sWHND_j-SFc",
+        },
+      }
+    );
+    const json = await response.json();
+    console.log(json);
+    console.log("loop " + i);
+    movieLst.push(json);
+  }
+  console.log(movieLst);
+  console.log(movieLst[0]);
+  movieWatchList = movieLst;
+  setLst([2]);
+
+  //setLst(["it should have chnaged by now"]);
+}
+
+function FocusMovie(props) {
+  console.log(props);
+  console.log(selectedMovie);
+  return <h1>{selectedMovie.original_title}</h1>;
 }
